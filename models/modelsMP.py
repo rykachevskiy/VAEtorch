c9e81cd5be10f0c9b32e8547bf6a25d21c318cbe
antonrykachevskiy <anton.rykachevskiy@yandex.ru>
2019-01-05 01:55:13 +0300

    asdasdasda

--- models/modelsMP.py
+++ models/modelsMP.py
@@ -18,26 +18,31 @@ class EncoderBlock(nn.Module):
     def __init__(self, inp_features, out_features):
         super(EncoderBlock, self).__init__()
         self.conv = nn.Conv2d(inp_features, out_features, (3, 3))#, padding=1)
+        self.mp = nn.MaxPool2d((2, 2), return_indices=True)
         self.bn = nn.BatchNorm2d(out_features)
 
     def forward(self, x):
-        return self.bn(self.conv(x))
+        x, mp_idx = self.mp(self.conv(x))
+        return self.bn(x), mp_idx
 
     def output_shape(self, inp_shape):
-        return inp_shape - 2
+        return (inp_shape - 2) // 2
 
 
 class DecoderBlock(nn.Module):
     def __init__(self, inp_features, out_features):
         super(DecoderBlock, self).__init__()
-        self.tconv = nn.ConvTranspose2d(inp_features, out_features, (3, 3))
+        self.upmp = nn.MaxUnpool2d((2, 2))
+        self.upconv = nn.ConvTranspose2d(inp_features, out_features, (3, 3), padding=1)
+
         self.bn = nn.BatchNorm2d(out_features)
 
     def forward(self, x):
-        return self.bn(self.tconv(x))
+        x, mp_idx = self.mp(self.conv(x))
+        return self.bn(x), mp_idx
 
     def output_shape(self, inp_shape):
-        return inp_shape + 2
+        return (inp_shape - 2) // 2
 
 
 class Encoder(nn.Module):
@@ -50,40 +55,7 @@ class Encoder(nn.Module):
                        EncoderBlock(8, 16),
                        EncoderBlock(16, 32)]
 
-        last_conv_shape = inp_shape
-        for b in self.blocks:
-            last_conv_shape = b.output_shape(last_conv_shape)
-
-        self.fc_shape = last_conv_shape[0] * last_conv_shape[1] * 32
-
-        self.fc_mu = nn.Linear(self.fc_shape, self.internal_shape)
-        self.fc_sigma = nn.Linear(self.fc_shape, self.internal_shape)
-
-        return last_conv_shape, self.fc_shape
-
-    def forward(self, x):
-        for block in self.blocks:
-            x = block(x)
-            x = nn.LeakyReLU(0.2)(x)
-
-        return self.fc_mu(x.reshape(-1, self.fc_shape)),\
-               self.fc_sigma(x.reshape(-1, self.fc_shape))
-
-
-
-class Decoder(nn.Module):
-    def __init__(self, internal_shape, fc_shape):
-        super(Decoder, self).__init__()
-
-        self.internal_shape = internal_shape
-        self.fc_shape = fc_shape
-
-        self.reshape_layer = ReshapeLayer(-1, )
-        self.blocks = [DecoderBlock(32, 16),
-                       DecoderBlock(16, 8),
-                       DecoderBlock(8, 1)]
-
-
+        self.fc_shape = inp_shape
         for b in self.blocks:
             self.fc_shape = b.output_shape(self.fc_shape)
 
@@ -92,6 +64,39 @@ class Decoder(nn.Module):
 
         self.fc_mu = nn.Linear(self.fc_shape, self.internal_shape)
         self.fc_sigma = nn.Linear(self.fc_shape, self.internal_shape)
+
+    def forward(self, x):
+        mp_idxs = []
+        for block in self.blocks:
+            x, mp_idx = block(x)
+            x = nn.LeakyReLU(0.2)(x)
+            mp_idxs.append(mp_idx)
+
+        return self.fc_mu(x.reshape(-1, self.fc_shape)),\
+               self.fc_sigma(x.reshape(-1, self.fc_shape)),\
+               mp_idx
+
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+
+        self.after_conv_shape = 7*7*16
+        self.internal_shape = 10
+        self.up_convs = nn.Sequential(nn.Linear(self.internal_shape, self.after_conv_shape),
+                                      nn.BatchNorm1d(self.after_conv_shape),
+                                      nn.LeakyReLU(0.1),
+                                      ReshapeLayer((-1, 16, 7, 7)),
+
+                                      nn.MaxUnpool2d((2,2)),
+                                      nn.Conv2d(16, 8, (3, 3), padding=1),
+                                      nn.LeakyReLU(0.1),
+                                      nn.BatchNorm2d(8),
+
+                                      nn.Upsample((28, 28)),
+                                      nn.Conv2d(8, 1, (3, 3), padding=1),
+                                      nn.Sigmoid())
 
     def simple_forward(self, x):
         return self.up_convs(x)
