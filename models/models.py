3993797f7af16a60aea1b2bca25d134af7d6f9e9
antonrykachevskiy <anton.rykachevskiy@yandex.ru>
2019-01-06 21:06:27 +0300

    working git

--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.utils.data
 from torch import nn
+from torch.nn import functional as F
 
 import numpy as np
 
@@ -19,22 +20,25 @@ class EncoderBlock(nn.Module):
         super(EncoderBlock, self).__init__()
         self.conv = nn.Conv2d(inp_features, out_features, (3, 3))#, padding=1)
         self.bn = nn.BatchNorm2d(out_features)
+        self.non_lin = nn.LeakyReLU(0.2)
 
     def forward(self, x):
-        return self.bn(self.conv(x))
+        return self.non_lin(self.bn(self.conv(x)))
 
     def output_shape(self, inp_shape):
         return inp_shape - 2
 
 
 class DecoderBlock(nn.Module):
-    def __init__(self, inp_features, out_features):
+    def __init__(self, inp_features, out_features, non_lin=nn.LeakyReLU(0.2)):
         super(DecoderBlock, self).__init__()
         self.tconv = nn.ConvTranspose2d(inp_features, out_features, (3, 3))
         self.bn = nn.BatchNorm2d(out_features)
+        self.non_lin = non_lin
+        #self.bn = lambda x:x
 
     def forward(self, x):
-        return self.bn(self.tconv(x))
+        return self.non_lin(self.bn(self.tconv(x)))
 
     def output_shape(self, inp_shape):
         return inp_shape + 2
@@ -46,25 +50,35 @@ class Encoder(nn.Module):
 
         self.internal_shape = internal_shape
 
-        self.blocks = [EncoderBlock(1, 8),
-                       EncoderBlock(8, 16),
-                       EncoderBlock(16, 32)]
+        self.blocks = nn.Sequential(EncoderBlock(1, 16),
+                                    EncoderBlock(16, 32),
+                                    EncoderBlock(32, 64))
 
-        last_conv_shape = inp_shape
+        self.last_conv_shape = inp_shape
         for b in self.blocks:
-            last_conv_shape = b.output_shape(last_conv_shape)
+            self.last_conv_shape = b.output_shape(self.last_conv_shape)
 
-        self.fc_shape = last_conv_shape[0] * last_conv_shape[1] * 32
+        self.fc_shape = self.last_conv_shape[0] * self.last_conv_shape[1] * 64
+
+        # #sanity check
+        # self.fc1 = nn.Linear(784, 400)
+        # self.fc21 = nn.Linear(400, 10)
+        # self.fc22 = nn.Linear(400, 10)
 
         self.fc_mu = nn.Linear(self.fc_shape, self.internal_shape)
         self.fc_sigma = nn.Linear(self.fc_shape, self.internal_shape)
 
-        return last_conv_shape, self.fc_shape
-
     def forward(self, x):
-        for block in self.blocks:
-            x = block(x)
-            x = nn.LeakyReLU(0.2)(x)
+        # #sanity check
+        # h1 = F.relu(self.fc1(x))
+        # return self.fc21(h1), self.fc22(h1)
+
+
+        # for block in self.blocks:
+        #     x = block(x)
+        #     x = nn.LeakyReLU(0.2)(x)
+
+        x = self.blocks(x)
 
         return self.fc_mu(x.reshape(-1, self.fc_shape)),\
                self.fc_sigma(x.reshape(-1, self.fc_shape))
@@ -72,82 +86,51 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, internal_shape, fc_shape):
+    def __init__(self, fc_shape, internal_shape):
         super(Decoder, self).__init__()
 
         self.internal_shape = internal_shape
         self.fc_shape = fc_shape
 
-        self.reshape_layer = ReshapeLayer(-1, )
-        self.blocks = [DecoderBlock(32, 16),
-                       DecoderBlock(16, 8),
-                       DecoderBlock(8, 1)]
+        self.fc = nn.Linear(internal_shape, 64 * fc_shape[0] * fc_shape[1])
+        self.reshape_layer = ReshapeLayer([-1, 64, self.fc_shape[0], self.fc_shape[1]])
+        self.blocks = nn.Sequential(DecoderBlock(64, 32),
+                                    DecoderBlock(32, 16),
+                                    DecoderBlock(16, 1, nn.Sigmoid()))
 
+        # self.fc3 = nn.Linear(10, 400)
+        # self.fc4 = nn.Linear(400, 784)
 
-        for b in self.blocks:
-            self.fc_shape = b.output_shape(self.fc_shape)
+    def forward(self, x):
+        # h3 = F.relu(self.fc3(x))
+        # return torch.sigmoid(self.fc4(h3))
 
-        print(self.fc_shape)
-        self.fc_shape = self.fc_shape[0] * self.fc_shape[1] * 32
+        x = self.fc(x)
+        x = self.reshape_layer(x)
+        x = nn.LeakyReLU(0.2)(x)
+        
+        # for b in self.blocks:
+        #     x = nn.LeakyReLU(0.2)(x)
+        #     x = b(x)
 
-        self.fc_mu = nn.Linear(self.fc_shape, self.internal_shape)
-        self.fc_sigma = nn.Linear(self.fc_shape, self.internal_shape)
-
-    def simple_forward(self, x):
-        return self.up_convs(x)
-
-    def forward(self, mu, log_sigma):
-        return self.simple_forward(self.reparametrize(mu, log_sigma))
-
-    def sample_hidden(self, batch_size=1):
-        z = torch.randn((batch_size, self.internal_shape))
-        return z
-
-    def reparametrize(self, mu, log_sigma):
-        batch_size=mu.shape[0]
-        return mu + self.sample_hidden(batch_size) * torch.exp(0.5*log_sigma) #std = torch.exp(0.5 * logvar)
-
-# class Decoder(nn.Module):
-#     def __init__(self):
-#         super(Decoder, self).__init__()
-#
-#         self.after_conv_shape = 7*7*16
-#         self.internal_shape = 100
-#         self.up_convs = nn.Sequential(nn.Linear(self.internal_shape, self.after_conv_shape),
-#                                       nn.BatchNorm1d(self.after_conv_shape),
-#                                       nn.LeakyReLU(0.1),
-#                                       ReshapeLayer((-1, 16, 7, 7)),
-#
-#                                       nn.Upsample((14,14)),
-#                                       nn.Conv2d(16, 8, (3, 3), padding=1),
-#                                       nn.LeakyReLU(0.1),
-#                                       nn.BatchNorm2d(8),
-#
-#                                       nn.Upsample((28, 28)),
-#                                       nn.Conv2d(8, 1, (3, 3), padding=1),
-#                                       nn.Sigmoid())
-#
-#     def simple_forward(self, x):
-#         return self.up_convs(x)
-#
-#     def forward(self, mu, log_sigma):
-#         return self.simple_forward(self.reparametrize(mu, log_sigma))
-#
-#     def sample_hidden(self, batch_size=1):
-#         z = torch.randn((batch_size, self.internal_shape))
-#         return z
-#
-#     def reparametrize(self, mu, log_sigma):
-#         batch_size=mu.shape[0]
-#         return mu + self.sample_hidden(batch_size) * torch.exp(0.5*log_sigma) #std = torch.exp(0.5 * logvar)
+        return self.blocks(x)
 
 
 class VAE(nn.Module):
-    def __init__(self):
+    def __init__(self, input_shape, internal_shape):
         super(VAE, self).__init__()
 
-        self.enc = Encoder()
-        self.dec = Decoder()
+        self.input_shape = np.array(list(input_shape))
+        self.internal_shape = internal_shape
+        self.enc = Encoder(self.input_shape, self.internal_shape)
+        self.dec = Decoder(self.enc.last_conv_shape, self.internal_shape)
 
     def forward(self, x):
-        pass
+        mu, logvar = self.enc(x)
+        z = self.reparametrize(mu, logvar)
+        restored_x = self.dec(z)
+        return restored_x, mu, logvar
+
+    def reparametrize(self, mu, logvar):
+        z = torch.randn_like(mu)
+        return z.mul(torch.exp(0.5 * logvar)).add_(mu)
